43a1a37d2721919af3d3fc010c382321567e85ec
jubustamantem <jubustamantem@unal.edu.co>
2022-10-04 07:45:23 -0500

    search resolvers

--- src/businessLogic/search/resolvers.js
+++ src/businessLogic/search/resolvers.js
@@ -2,9 +2,45 @@ import searchRequests from '../../restConsumption/search/requests';
 
 const searchResolvers = {
     Query: {
+        searchAllCampus: (_) => {
+            return searchRequests.searchAllCampus(_)
+        },
+        searchAllFaculties: (_) => {
+            return searchRequests.searchAllFaculties(_)
+        },
+        searchAllSubjects: (_) => {
+            return searchRequests.searchAllSubjects(_)
+        },
+        searchSubjectByName: (_, { courseName }) => {
+            return searchRequests.searchSubjectByName(_, { courseName })
+        },
+        searchSubjectByType: (_, { courseType }) => {
+            return searchRequests.searchSubjectByType(_, { courseType })
+        },
+        searchSubjectById: (_, { courseId }) => {
+            return searchRequests.searchSubjectById(_, { courseId })
+        },
+        searchSubjectByKeyword: (_, { keyword }) => {
+            return searchRequests.searchSubjectByKeyword(_, { keyword })
+        },
+        searchmateriaByPlan: (_, { Id }) => {
+            return searchRequests.searchmateriaByPlan(_, { Id })
+        },
+        groupBynumer: (_, { courseName }) => {
+            return searchRequests.groupBynumer(_, { groupNumber })
+        },
         searchByProfessor: (_, { teacherName }) => {
             return searchRequests.searchByProfessor(_, {teacherName})
-        }
+        },
+        groupByQuota: (_) => {
+            return searchRequests.groupByQuota(_)
+        },
+        groupById: (_, { groupId }) => {
+            return searchRequests.groupById(_, { groupId })
+        },
+        subGrupoByGrupoId: (_, { subGroupId }) => {
+            return searchRequests.subGrupoByGrupoId(_, { subGroupId })
+        },
     }
 }
 
--- src/restConsumption/search/requests.js
+++ src/restConsumption/search/requests.js
@@ -5,8 +5,47 @@ import request from "request-promise-native";
 const URL = `http://${url}:${port}/${entryPoint}`;
 
 const requests = {
+    searchAllCampus: (_) =>
+        generalRequest(`${URL}/sedes`, 'GET'),
+
+    searchAllFaculties: (_) =>
+        generalRequest(`${URL}/facultades`, 'GET'),
+
+    searchAllSubjects: (_) =>
+        generalRequest(`${URL}/materias`, 'GET'),
+
+    searchSubjectByName: (_, { courseName }) =>
+        generalRequest(`${URL}/nombre/${courseName}`, 'GET'),
+
+    searchSubjectByCode: (_, { courseCode }) =>
+        generalRequest(`${URL}/codigo/${courseCode}`, 'GET'),
+
+    searchSubjectByType: (_, { courseType }) =>
+        generalRequest(`${URL}/tipologia/${courseType}`, 'GET'),
+
+    searchSubjectById: (_, { courseId }) =>
+        generalRequest(`${URL}/id/${courseId}`, 'GET'),
+
+    searchSubjectByKeyword: (_, { keyword }) =>
+        generalRequest(`${URL}/keyword/${keyword}`, 'GET'),
+
+    searchmateriaByPlan: (_, { Id }) =>
+        generalRequest(`${URL}/materiaByPlan/${Id}`, 'GET'),
+
+    groupBynumer: (_, { groupNumber }) =>
+        generalRequest(`${URL}/groupBynumer/${groupNumber}`, 'GET'),
+
     searchByProfessor: (_, { teacherName }) =>
-        generalRequest(`${URL}/groupByProfessor/${teacherName}`, 'GET')
+        generalRequest(`${URL}/groupByProfessor/${teacherName}`, 'GET'),
+
+    groupByQuota: (_) =>
+        generalRequest(`${URL}/groupByQuota`, 'GET'),
+
+    groupById: (_, {groupId}) =>
+        generalRequest(`${URL}/groupById/${groupId}`, 'GET'),
+
+    subGrupoByGrupoId: (_, {subGroupId}) =>
+        generalRequest(`${URL}/subGrupoByGrupoId/${subGroupId}`, 'GET')
 };
 
 export default requests;
